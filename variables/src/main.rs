bd7f201087cab803cb06e668351bc702063696c1
RbertKo <myeongsku@gmail.com>
2020-10-13 21:58:22 +0900

    Shadow

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -8,4 +8,12 @@ fn main() {
     println!("x의 값: {}", x);
 
     println!("미래 예진이 연봉: {}", YEJIN_ANNUAL_INCOME);
+
+    // Shadowed
+    let y = 10;
+    println!("y의 값: {}", y);
+
+    // Shadowd 하다.
+    let y = 12;
+    println!("새로운 y의 값: {}", y);
 }
