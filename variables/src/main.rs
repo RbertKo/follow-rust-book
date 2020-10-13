a2c4df92c2f29f67be7c02a505d093cc3787c9fc
RbertKo <myeongsku@gmail.com>
2020-10-13 21:44:59 +0900

    Variable exmaple

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,11 @@
+const YEJIN_ANNUAL_INCOME: u32 = 1200000000;
+
 fn main() {
     let mut x = 5;
     println!("x의 값: {}", x);
 
     x = 7;
     println!("x의 값: {}", x);
+
+    println!("미래 예진이 연봉: {}", YEJIN_ANNUAL_INCOME);
 }
