a410e5fbaccf0f8ebc1645e68088412890dfa2a8
RbertKo <myeongsku@gmail.com>
2020-10-16 02:15:58 +0900

    i8 - i32 type

--- data_type/src/main.rs
+++ data_type/src/main.rs
@@ -1,5 +1,19 @@
 fn main() {
-    let guess: u32 = "42".parse().expect("숫자가 아닙니다!");
+    // 8bit Integer
+    let min_int_8: i8 = i8::MIN;
+    let max_int_8: i8 = i8::MAX;
     
-    println!("guess: {}", guess);
+    println!("8bit Integer is a value between {} and {}", min_int_8, max_int_8);
+
+    // 16bit Integer
+    let min_int_16: i16 = i16::MIN;
+    let max_int_16: i16 = i16::MAX;
+
+    println!("16bit Integer is a value between {} and {}", min_int_16, max_int_16);
+
+    // 32bit Integer
+    let min_int_32: i32 = i32::MIN;
+    let max_int_32: i32 = i32::MAX;
+
+    println!("32bit Integer is a value between {} and {}", min_int_32, max_int_32);
 }
