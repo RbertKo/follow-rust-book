a09a15a33a20aaa084d736c48edcc91605e53084
RbertKo <myeongsku@gmail.com>
2020-10-16 02:29:48 +0900

    floating point

--- data_type/src/main.rs
+++ data_type/src/main.rs
@@ -1,3 +1,7 @@
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
+
 fn main() {
     // 8bit Integer
     let min_int_8: i8 = i8::MIN;
@@ -16,4 +20,19 @@ fn main() {
     let max_int_32: i32 = i32::MAX;
 
     println!("32bit Integer is a value between {} and {}", min_int_32, max_int_32);
+
+    // isize Integer => It changes depending on the memory you use.
+    let min_int_isize: isize = isize::MIN;
+    let max_int_isize: isize = isize::MAX;
+
+    println!("isize Intefer is a value between {} and {}. It Changes depending on the memory you use.", min_int_isize, max_int_isize); // My System used 64bit
+
+    // floating point. Rust's default is f64.
+    let any_float = 1.0;
+
+    print_type_of(&any_float); // f64
+
+    let f32_float: f32 = 1.0;
+
+    print_type_of(&f32_float);
 }
