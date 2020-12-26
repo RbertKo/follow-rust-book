c5f6a58265fd0fdee72adcd0c2ffc532b30ce9a9
R.bertKo <myeongsku@gmail.com>
2020-12-26 18:08:09 +0900

    add new function with  parameter

--- function/src/main.rs
+++ function/src/main.rs
@@ -2,8 +2,15 @@ fn main() {
     println!("Hello, world!");
 
     another_function();
+    another_function_2(20);
 }
 
+// Define new functino fusing 'fn' keyword
 fn another_function() {
     println!("Merry X-Mas")
+}
+
+// Add Parameters
+fn another_function_2(count: i32) {
+    println!("{}th Christmas", count)
 }
