3a3a4806fe8b53a74dd38d81d38604ca2eaa4178
RbertKo <myeongsku@gmail.com>
2021-01-04 03:44:04 +0900

    if expressions. assign if expressions to number varibale

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -3,11 +3,13 @@ fn main() {
 
     let number = 5;
 
-    if number < 3 {
-        println!("number is less than 3: number = {}", number)
+    let number = if number < 3 { // like ternary operator
+        println!("number is less than 3: number = {}", number);
+        number
     } else {
-        println!("number is greater than 3: number = {}", number)
-    }
+        println!("number is greater than 3: number = {}", number);
+        3
+    };
 
     // if number { => Error. Rust will not automatically try to convert non-Boolean types to a Boolean.
     //     println!("number is existed.")
