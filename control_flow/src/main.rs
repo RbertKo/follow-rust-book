e50287b097cdf60626f94ed2479fde33b0188c2f
RbertKo <myeongsku@gmail.com>
2021-01-04 03:34:41 +0900

    if expressions 2

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -8,4 +8,12 @@ fn main() {
     } else {
         println!("number is greater than 3: number = {}", number)
     }
+
+    // if number { => Error. Rust will not automatically try to convert non-Boolean types to a Boolean.
+    //     println!("number is existed.")
+    // }
+    // => Good case
+    if number != 3 {
+        println!("number is not 3: number = {}", number);
+    }
 }
