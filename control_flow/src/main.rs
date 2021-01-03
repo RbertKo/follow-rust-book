c7b01988fc465a674c2aa31b5314666d418c5536
RbertKo <myeongsku@gmail.com>
2021-01-04 03:31:28 +0900

    if expressions 1

---
+++ control_flow/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // if expressions
+
+    let number = 5;
+
+    if number < 3 {
+        println!("number is less than 3: number = {}", number)
+    } else {
+        println!("number is greater than 3: number = {}", number)
+    }
+}
