d42b831152778d54e3e4c6f54b1ca571f47d8f07
RbertKo <myeongsku@gmail.com>
2021-01-07 00:07:04 +0900

    Add comment about 'borrow'

--- reference_borrowing/src/main.rs
+++ reference_borrowing/src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
     let s1 = String::from("hello");
 
-    let len = calculate_length(&s1); // This(&) is called reference
+    let len = calculate_length(&s1); // This(&) is called 'reference'
 
     println!("The length of '{}' is {}.", s1, len);
 }
 
-fn calculate_length(s: &String) -> usize {
+fn calculate_length(s: &String) -> usize { // This(&) is called 'borrow'
     s.len()
 }
