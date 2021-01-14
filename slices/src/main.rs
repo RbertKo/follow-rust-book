eb11c2144d56fa49816552637c7acd2f6b7ecec3
RbertKo <myeongsku@gmail.com>
2021-01-14 22:43:17 +0900

    Shardow 's' after borrow

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,14 +1,18 @@
 fn main() {
     let s = String::from("Hello, world!");
+    let s2 = &s[..];
 
     let len = first_word(&s);
 
     let hello = &s[..len];
     let world = &s[7..];
 
+    let s = "&s.push('s');";
+
     println!("{}", hello);
     println!("{}", world);
     println!("{}", &s[..]); // a slice of the entire string
+    println!("{}", s2)
 }
 
 fn first_word(s: &String) -> usize {
