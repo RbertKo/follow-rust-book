f66497f3bd405d565b83edaaa90cca44dc95ce6a
RbertKo <myeongsku@gmail.com>
2021-01-16 02:01:57 +0900

    Create instance of the struct

--- define_struct/src/main.rs
+++ define_struct/src/main.rs
@@ -6,5 +6,10 @@ struct User {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let user = User {
+        username: String::from("MyeongSeok Ko"),
+        email: String::from("myeongsku@gmail.com"),
+        sign_in_count: 10,
+        active: true
+    }
 }
