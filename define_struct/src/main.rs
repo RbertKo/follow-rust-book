20aacf60dea73aa846a7378cfb37986baa127152
RbertKo <myeongsku@gmail.com>
2021-01-16 02:04:47 +0900

    Add comment about how to use specify property

--- define_struct/src/main.rs
+++ define_struct/src/main.rs
@@ -11,5 +11,7 @@ fn main() {
         email: String::from("myeongsku@gmail.com"),
         sign_in_count: 10,
         active: true
-    }
+    };
+
+    // user.active = false; -> 가변 변수일 경우 다른 값 할당 가능
 }
