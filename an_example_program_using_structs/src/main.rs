a56de29ae969b3451536ed05e695c8d14daed028
RbertKo <myeongsku@gmail.com>
2021-01-21 23:53:08 +0900

    Get area of rectangles

--- an_example_program_using_structs/src/main.rs
+++ an_example_program_using_structs/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let length1 = 50;
+    let width1 = 30;
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(length1, width1)
+    );
+}
+
+fn area(length: u32, width: u32) -> u32 {
+    length * width
 }
