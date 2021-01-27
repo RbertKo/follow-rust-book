be122d01578278b2f56e54ddc8c515faab1da487
RbertKo <myeongsku@gmail.com>
2021-01-28 02:11:46 +0900

    Change V4 like tuple

--- enum_value/src/main.rs
+++ enum_value/src/main.rs
@@ -1,11 +1,11 @@
 #[derive(Debug)]
 enum IpAddrKind {
-    V4(String), // This is called Variants of enums in rust
+    V4(u8, u8, u8, u8), // This is called Variants of enums in rust
     V6(String),
 }
 
 fn main() {
-    let four = IpAddrKind::V4(String::from("127.0.0.1"));
+    let four = IpAddrKind::V4(127, 0, 0, 1);
     
     let six = IpAddrKind::V6(String::from("::1"));
 
