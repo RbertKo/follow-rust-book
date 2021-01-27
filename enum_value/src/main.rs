ae0cf7c946e26535fb0450439b65f65fbdd868cf
RbertKo <myeongsku@gmail.com>
2021-01-28 02:19:00 +0900

    Create impl of Message

--- enum_value/src/main.rs
+++ enum_value/src/main.rs
@@ -4,10 +4,27 @@ enum IpAddrKind {
     V6(String),
 }
 
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+       println!("call")
+    }
+}
+
 fn main() {
     let four = IpAddrKind::V4(127, 0, 0, 1);
     
     let six = IpAddrKind::V6(String::from("::1"));
 
     println!("four: {:?} / six: {:?}", four, six);
+
+    let m = Message::Write(String::from("hello"));
+
+    m.call();
 }
