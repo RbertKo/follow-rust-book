29484101c4b95de7e27d6188d18283567e197545
RbertKo <myeongsku@gmail.com>
2021-02-07 20:23:05 +0900

    Use if let syntax

--- if_let_control_flow/src/main.rs
+++ if_let_control_flow/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let some_u8_value: Option<u32> = Some(3);
+    
+    // Using match
+    match some_u8_value {
+        Some(3) => println!("Oh, this is three."),
+        _ => println!("Can't read this value.")
+    }
+
+    // Using if let syntax
+    if let Some(3) = some_u8_value {
+        println!("Oh, this is three.");
+    } else {
+        println!("Can't read this value.");
+    }
 }
