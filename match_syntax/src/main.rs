29a1268737e1a4f9a781eb4f6d494dfc2641129a
RbertKo <myeongsku@gmail.com>
2021-02-07 12:36:07 +0900

    matching with Option<t>

--- match_syntax/src/main.rs
+++ match_syntax/src/main.rs
@@ -27,7 +27,26 @@ fn value_in_cents(coin: Coin) -> u32 {
   }
 }
 
+fn chip_in(wallet: Option<u32>) -> Option<u32> {
+  match wallet {
+    None => Some(1000),
+    Some(money) => Some(money + 1000),
+  }
+}
+
 fn main() {
     value_in_cents(Coin::Penny);
     value_in_cents(Coin::Quarter(Color::Gold));
+
+    let my_wallet1: Option<u32> = Some(3000);
+    let my_wallet2: Option<u32> = None; 
+
+    println!("my wallet 1: {:?}", my_wallet1);
+    println!("my wallet 2: {:?}", my_wallet2);
+
+    let my_wallet1 = chip_in(my_wallet1);
+    let my_wallet2 = chip_in(my_wallet2);
+
+    println!("my wallet 1: {:?}", my_wallet1);
+    println!("my wallet 2: {:?}", my_wallet2);
 }
