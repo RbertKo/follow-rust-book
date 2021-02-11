7d0ef9610cd17b33c90f4f3b679f5552e25e1208
RbertKo <myeongsku@gmail.com>
2021-02-11 23:49:47 +0900

    Define a lots of mod

--- communicator/src/lib.rs
+++ communicator/src/lib.rs
@@ -1,12 +1,14 @@
 mod client {
     fn connect() {
-
-    }
-
-    mod network {
-        fn connect() {
-            
-        }
     }
 }
 
+mod network {
+    fn connect() {
+    }
+
+    mod server {
+        fn connect() {
+        }
+    }
+}
