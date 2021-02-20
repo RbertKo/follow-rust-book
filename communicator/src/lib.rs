77fff125df0725f82370823bd6ae06911b8eb543
R.bertKo <myeongsku@gmail.com>
2021-02-20 20:10:04 +0900

    Fix module loading issue

--- communicator/src/lib.rs
+++ communicator/src/lib.rs
@@ -4,6 +4,10 @@ pub mod network;
 
 #[cfg(test)]
 mod tests {
+    use super::client;
+    // super는 부모
+    // ::는 루트... 하지만 해당 부분에서 use ::client; 를 사용했을 때 동작이 안 된다.
+    
     #[test]
     fn it_works() {
         client::connect();
