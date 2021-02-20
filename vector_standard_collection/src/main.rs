256ccfbdef404f2fd136708608a7ac7b649292c5
R.bertKo <myeongsku@gmail.com>
2021-02-21 00:37:37 +0900

    read an element of a vector instance

--- vector_standard_collection/src/main.rs
+++ vector_standard_collection/src/main.rs
@@ -9,5 +9,15 @@ fn main() {
 
     {
         let vec_scope = vec![1, 2, 3];
+
+        let third: &i32 = &vec_scope[2];
+        let third_option: Option<&i32> = vec_scope.get(2);
+
+        println!("{}", third);
+        println!("{}", if let None = third_option {
+            0
+        } else {
+            vec_scope[2]
+        })
     } // free vec_scope and all elements
 }
