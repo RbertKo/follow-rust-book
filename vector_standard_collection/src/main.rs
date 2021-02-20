1dd0d10b56047b4894ce448f58fd90e810e53ffc
R.bertKo <myeongsku@gmail.com>
2021-02-20 21:03:43 +0900

    create a new instance of the vector for pushing some element

--- vector_standard_collection/src/main.rs
+++ vector_standard_collection/src/main.rs
@@ -2,4 +2,8 @@ fn main() {
     let vec: Vec<i32> = Vec::new();
 
     let used_vec = vec![1, 2, 3];
+
+    let mut push_vec = Vec::new(); // It return error. if you not push some element.
+
+    push_vec.push(1); 
 }
