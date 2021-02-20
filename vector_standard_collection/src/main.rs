0d67d6a0018aa3de8c15e9b2a39cc46c612c5d01
R.bertKo <myeongsku@gmail.com>
2021-02-21 00:16:56 +0900

    vector scope

--- vector_standard_collection/src/main.rs
+++ vector_standard_collection/src/main.rs
@@ -6,4 +6,8 @@ fn main() {
     let mut push_vec = Vec::new(); // It return error. if you not push some element.
 
     push_vec.push(1); 
+
+    {
+        let vec_scope = vec![1, 2, 3];
+    } // free vec_scope and all elements
 }
