d883e4c0fdb67a2e3d8819ec59ecba10730212cc
R.bertKo <myeongsku@gmail.com>
2021-02-21 00:41:20 +0900

    update condition

--- vector_standard_collection/src/main.rs
+++ vector_standard_collection/src/main.rs
@@ -11,13 +11,13 @@ fn main() {
         let vec_scope = vec![1, 2, 3];
 
         let third: &i32 = &vec_scope[2];
-        let third_option: Option<&i32> = vec_scope.get(2);
+        let third_option: Option<&i32> = vec_scope.get(5);
 
         println!("{}", third);
-        println!("{}", if let None = third_option {
-            0
+        println!("{}", if let Some(element) = third_option {
+            element
         } else {
-            vec_scope[2]
+            &0
         })
     } // free vec_scope and all elements
 }
