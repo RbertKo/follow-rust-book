aced054a5b70feb7d6411c63afbb920adf5554bb
R.bertKo <myeongsku@gmail.com>
2021-02-21 10:40:51 +0900

    Declare s4 using String::from

--- string_standard_collection/src/main.rs
+++ string_standard_collection/src/main.rs
@@ -5,7 +5,10 @@ fn main() {
 
     let s3 = s2.to_string(); // Declare s3: String from s2
 
+    let s4 = String::from("like a declaring s3 from s2");
+
     println!("s1: {}", s1);
     println!("s2: {}", s2);
     println!("s3: {}", s3);
+    println!("s4: {}", s4);
 }
