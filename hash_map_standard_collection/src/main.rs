6b16de4d936c6e0c841c361f0fc422dfe254cd18
RbertKo <myeongsku@gmail.com>
2021-02-28 18:48:04 +0900

    Create hash_map using collect

--- hash_map_standard_collection/src/main.rs
+++ hash_map_standard_collection/src/main.rs
@@ -5,4 +5,11 @@ fn main() {
 
     scores.insert("blue", 10);
     scores.insert("yellow", 50);
+
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+
+    let socres2: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+
+    println!("{:?}", socres2);
 }
