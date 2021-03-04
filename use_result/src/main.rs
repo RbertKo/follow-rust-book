14ace2758ccdbfaf0563a4d8d648b791ae446db4
R.bertKo <myeongsku@gmail.com>
2021-03-04 22:19:20 +0900

    Add get current working dir logic

--- use_result/src/main.rs
+++ use_result/src/main.rs
@@ -1,7 +1,24 @@
 use std::fs::File;
+use std::env;
 
 fn main() {
-    let f = File::open("./hello.txt");
+    let current_dir = env::current_dir();
+    
+    let current_dir = if let Ok(path) = &current_dir { 
+        let result = if let Some(str_path) = path.to_str() {
+            str_path
+        } else {
+            panic!("It's not current working dir");
+        };
+
+        format!("{}/{}", result, "hello.txt")
+    } else {
+        panic!("panic when get path {:?}", &current_dir);
+    };
+
+    println!("{:?}", current_dir);
+
+    let f = File::open(current_dir);
 
     let f = match f {
         Ok(file) => file,
@@ -10,5 +27,5 @@ fn main() {
         }
     };
 
-    println!("file is '{:?}'", f);
+    println!("file is '{:?}'", f.to_str());
 }
