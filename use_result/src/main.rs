065f6371358e06fb7ba1f064b05e33320cd8a5c1
R.bertKo <myeongsku@gmail.com>
2021-03-04 21:57:59 +0900

    Result match

--- use_result/src/main.rs
+++ use_result/src/main.rs
@@ -1,5 +1,14 @@
 use std::fs::File;
 
 fn main() {
-    let f: u32 = File::open("hello.txt");
+    let f = File::open("./hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => {
+            panic!("파일을 여는 도중 문제가 발생했습니다: {:?}", error)
+        }
+    };
+
+    println!("file is '{:?}'", f);
 }
