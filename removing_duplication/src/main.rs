73252f6e8771e7cf28fbdecd8ad889a80a01efd5
RbertKo <myeongsku@gmail.com>
2021-03-10 23:55:18 +0900

    Add exmaple code to find the largest number in two lists of numbers

--- removing_duplication/src/main.rs
+++ removing_duplication/src/main.rs
@@ -10,4 +10,16 @@ fn main() {
     }
 
     println!("The largest number is {}", largest);
+
+    let numbers = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    let mut largest = numbers[0];
+
+    for number in numbers {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    println!("The largest number is {}", largest);
 }
