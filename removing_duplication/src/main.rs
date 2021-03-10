194e5587bb392f8ccff318ec8fae58aea7859621
RbertKo <myeongsku@gmail.com>
2021-03-10 23:54:04 +0900

    Add exmaple code to find the largest number in a list of numbers

--- removing_duplication/src/main.rs
+++ removing_duplication/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let numbers = vec![34, 50, 25, 100, 65];
+
+    let mut largest = numbers[0];
+
+    for number in numbers {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    println!("The largest number is {}", largest);
 }
