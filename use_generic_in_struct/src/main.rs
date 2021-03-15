0708654646299f2aa0c3e743bfab1d8c4322f1d8
RbertKo <myeongsku@gmail.com>
2021-03-15 23:59:37 +0900

    Add generic impl

--- use_generic_in_struct/src/main.rs
+++ use_generic_in_struct/src/main.rs
@@ -8,6 +8,12 @@ struct PointV2<T, U> {
     y: U,
 }
 
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
 fn main() {
     let integer = Point { x: 5, y: 10 };
     let float = Point { x: 1.0, y: 4.0 };
@@ -15,4 +21,6 @@ fn main() {
     // let test = Point { x: 5.0, y: 10 }; -> 같은 타입이 아니면 오류 발생
 
     let union_point = PointV2 { x: 5, y: 1.0 };
+
+    println!("intefer.x = {}", integer.x);
 }
