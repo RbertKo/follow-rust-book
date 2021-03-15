5ed00b054e551a3d90c0e86c89a9ffd2df25b8eb
RbertKo <myeongsku@gmail.com>
2021-03-15 23:51:14 +0900

    Multiple generic type

--- use_generic_in_struct/src/main.rs
+++ use_generic_in_struct/src/main.rs
@@ -3,9 +3,16 @@ struct Point<T> {
     y: T,
 }
 
+struct PointV2<T, U> {
+    x: T,
+    y: U,
+}
+
 fn main() {
     let integer = Point { x: 5, y: 10 };
     let float = Point { x: 1.0, y: 4.0 };
 
     // let test = Point { x: 5.0, y: 10 }; -> 같은 타입이 아니면 오류 발생
+
+    let union_point = PointV2 { x: 5, y: 1.0 };
 }
