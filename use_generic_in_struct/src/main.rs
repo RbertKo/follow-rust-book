1457b2c3a7f0b686edc20222e8308c01c6b5f0dd
RbertKo <myeongsku@gmail.com>
2021-03-15 23:47:48 +0900

    Add comment about bad case

--- use_generic_in_struct/src/main.rs
+++ use_generic_in_struct/src/main.rs
@@ -6,4 +6,6 @@ struct Point<T> {
 fn main() {
     let integer = Point { x: 5, y: 10 };
     let float = Point { x: 1.0, y: 4.0 };
+
+    // let test = Point { x: 5.0, y: 10 }; -> 같은 타입이 아니면 오류 발생
 }
