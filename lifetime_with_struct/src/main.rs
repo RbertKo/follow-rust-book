3f42080ed8b612c584eee40f272c931f699144f5
RbertKo <myeongsku@gmail.com>
2021-03-27 19:39:03 +0900

    use lifetime on struct

--- lifetime_with_struct/src/main.rs
+++ lifetime_with_struct/src/main.rs
@@ -1,3 +1,12 @@
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let novel = String::from("Call me RbertKo. Some years ago...");
+    let first_sentence = novel.split('.')
+        .next()
+        .expect("could not find a '.'");
+
+    let i = ImportantExcerpt { part: first_sentence };
 }
