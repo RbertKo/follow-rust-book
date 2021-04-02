ce4c80296ff430ecf519b391746a8934b80432cf
R.bertKo <myeongsku@gmail.com>
2021-04-02 23:58:53 +0900

    Add Ractangle struct

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,3 +1,15 @@
+#[derive(Debug)]
+pub struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+impl Rectangle {
+    pub fn can_hold(&self, other: &Rectangle) -> bool {
+        self.length > other.length && self.width > other.width
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
