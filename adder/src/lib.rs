b48c9393a10a8eca4b92f58dcf9dc45b481dcc63
RbertKo <myeongsku@gmail.com>
2021-04-07 23:55:37 +0900

    custom assert

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -10,6 +10,10 @@ impl Rectangle {
     }
 }
 
+pub fn greeting(name: &str) -> String {
+    String::from("Hello!")
+}
+
 pub fn add_two(a: i32) -> i32 {
     a + 2
 }
@@ -39,5 +43,14 @@ mod tests {
     #[test]
     fn it_adds_two() {
         assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`", result
+        );
     }
 }
