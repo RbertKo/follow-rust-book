46b2b3af75d84ac3c310960943b7dadddc3caeda
RbertKo <myeongsku@gmail.com>
2021-05-05 21:09:43 +0900

    Fix some issue

--- greps/src/main.rs
+++ greps/src/main.rs
@@ -1,29 +1,25 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let mut f = File::open(filename).expect("file not found");
+    let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents).expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
-}
-
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
-
-    (query, filename)
 }
 
 struct Config {
@@ -32,7 +28,7 @@ struct Config {
 }
 
 impl Config { 
-    fn new(args: std::env::Args) -> Result<Config, &'static str> {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("not enough arguments");
         }
