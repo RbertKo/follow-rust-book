3f840c7ca5dd0758a9d95b5a88a153670c5d690d
RbertKo <myeongsku@gmail.com>
2021-05-06 23:35:55 +0900

    Refator

--- greps/src/main.rs
+++ greps/src/main.rs
@@ -28,13 +28,20 @@ struct Config {
 }
 
 impl Config { 
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        
-        let query = args[1].clone();
-        let filename = args[2].clone();
+    fn new(args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
         Ok(Config { query, filename })
     }
