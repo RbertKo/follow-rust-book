b04c4e18d48784b12e4ce8a1471556ae9a0aac25
RbertKo <myeongsku@gmail.com>
2021-05-07 21:17:48 +0900

    Add run function

--- greps/src/main.rs
+++ greps/src/main.rs
@@ -14,10 +14,14 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
+    run(config);
+}
+
+fn run(config: Config) {
     let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
-    f.read_to_string(&mut contents).expect("Something went wrong reading the file");
+    f.read_to_string(&mut contents).expect("something went wrong reading the file");
 
     println!("With text:\n{}", contents);
 }
