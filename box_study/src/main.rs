ea98fe0d776ab9eed75301d4e8e3a61fae2b0a8e
RbertKo <myeongsku@gmail.com>
2021-06-24 23:41:50 +0900

    Add cons list

--- box_study/src/main.rs
+++ box_study/src/main.rs
@@ -1,5 +1,10 @@
-fn main() {
-     let a = Box::new(10);
+enum List {
+     Cons(i32, Box<List>),
+     Nil,
+ }
 
-     println!("{}", a);
+use List::{Cons, Nil};
+
+fn main() {
+     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 }
