b12abef28b0fb7dee5dbc420a734921af90c4d8e
RbertKo <myeongsku@gmail.com>
2021-09-12 23:51:20 +0900

    create multiple constructors

--- message_passing/src/main.rs
+++ message_passing/src/main.rs
@@ -3,23 +3,42 @@ use std::sync::mpsc;
 use std::time::Duration;
 
 fn main() {
-    let (tx, rx) = mpsc::channel();
+// --snip--
 
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("hi"),
-            String::from("from"),
-            String::from("the"),
-            String::from("thread"),
-        ];
+let (tx, rx) = mpsc::channel();
 
-        for val in vals {
-            tx.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
+let tx1 = mpsc::Sender::clone(&tx);
+thread::spawn(move || {
+    let vals = vec![
+        String::from("hi"),
+        String::from("from"),
+        String::from("the"),
+        String::from("thread"),
+    ];
 
-    for received in rx {
-        println!("Got: {}", received);
+    for val in vals {
+        tx1.send(val).unwrap();
+        thread::sleep(Duration::from_secs(1));
     }
+});
+
+thread::spawn(move || {
+    let vals = vec![
+        String::from("more"),
+        String::from("messages"),
+        String::from("for"),
+        String::from("you"),
+    ];
+
+    for val in vals {
+        tx.send(val).unwrap();
+        thread::sleep(Duration::from_secs(1));
+    }
+});
+
+for received in rx {
+    println!("Got: {}", received);
+}
+
+// --snip--
 }
